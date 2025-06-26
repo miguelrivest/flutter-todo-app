4678630e2feba1b75bbe135e1c067c4e3f00ebb5
Miguel Rivest <miguelrivest@outlook.com>
2025-06-25 21:46:58 -0400

    Add feature to edit current todos

--- lib/src/features/todo/widgets/todo_item_widget.dart
+++ lib/src/features/todo/widgets/todo_item_widget.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/features/todo/models/todo_item.dart';
 
-class ToDoItemWidget extends StatelessWidget {
+class ToDoItemWidget extends StatefulWidget {
   final ToDoItem todo;
 
   const ToDoItemWidget({super.key, required this.todo});
 
+  @override
+  State<ToDoItemWidget> createState() => _ToDoItemWidgetState();
+}
+class _ToDoItemWidgetState extends State<ToDoItemWidget> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -13,29 +17,45 @@ class ToDoItemWidget extends StatelessWidget {
       leading: Checkbox(
         value: todo.isCompleted,
         onChanged: (value) {
-          todo.isCompleted = value!;
-          todo.save();
+          setState(() {
+            widget.todo.isCompleted = value!;
+            widget.todo.save();
+          });
         },
       ),
-      title: Text(
-        todo.title,
+      title: TextField(
+        controller: TextEditingController(text: widget.todo.title),
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+        ),
+        onSubmitted: (value) {
+          setState(() {
+            widget.todo.title = value;
+            widget.todo.save();
+          });
+        },
         style: TextStyle(
           fontSize: 16,
           color: Theme.of(context).colorScheme.onSurface,
-          decoration: todo.isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+          decoration: widget.todo.isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
           decorationColor: Theme.of(context).colorScheme.onSurface,
         ),
       ),
       trailing: IconButton(
         icon: const Icon(Icons.delete),
         onPressed: () {
-          todo.delete();
+          widget.todo.delete();
         },
       ),
       onTap: () {
-        todo.isCompleted = !todo.isCompleted;
-        todo.save();
+        setState(() {
+          widget.todo.isCompleted = !widget.todo.isCompleted;
+          widget.todo.save();
+        });
       },
     );
   }
+
+  ToDoItem get todo => widget.todo;
 }
+
